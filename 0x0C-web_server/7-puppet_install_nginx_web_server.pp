4072bd8e6c7f1934e559371a9d283b0ee669686e
leila111111 <leila_merzak@outlook.fr>
2023-09-27 00:11:30 +0000

    task5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,14 @@
 # Install Nginx web server (w/ Puppet)
 
+include stdlib
+
 exec {'installation':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo 'Hello World!' > /var/www/html/index.nginx-debian.html ; sudo sed -i '/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4/ permanent;' /etc/nginx/sites-available/default ; sudo service nginx start',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo 'Hello World!' > /var/www/html/index.nginx-debian.html ; sudo service nginx start',
+}
+
+file_line { '301':
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => '    location /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}',
 }
