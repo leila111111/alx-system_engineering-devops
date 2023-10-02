65990b16c49516ae79d84291aad8cc7288a100a5
leila111111 <leila_merzak@outlook.fr>
2023-10-02 21:51:47 +0000

    taskpuppet0

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,15 @@
-# Puppet manifest creating a custom HTTP header response
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
+# automate the task of creating a custom HTTP header response, but with Puppet.
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
 }
 
 package { 'nginx':
   ensure  => installed,
-  require => Exec['apt-get-update'],
+  require => Exec['update'],
 }
 
-file_line { 'a':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
-}
-
-file_line { 'b':
+file_line { 'host':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
@@ -24,12 +17,7 @@ file_line { 'b':
   require => Package['nginx'],
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec { 'nginx restart':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
