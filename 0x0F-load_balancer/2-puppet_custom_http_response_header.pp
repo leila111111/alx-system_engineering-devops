4268df2cdfec2d838011888844699ba34de637c8
leila111111 <leila_merzak@outlook.fr>
2023-10-02 21:56:29 +0000

    taskpuppet0

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,7 +13,7 @@ file_line { 'host':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $HOSTNAME;',
+  line    => 'add_header X-Served-By $(hostname);',
   require => Package['nginx'],
 }
 
