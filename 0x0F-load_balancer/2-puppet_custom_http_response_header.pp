ba977c48479e81da6f719a155444ec741e6e2b46
leila111111 <leila_merzak@outlook.fr>
2023-10-15 23:12:52 +0000

    tasks

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,24 @@
 # automate the task of creating a custom HTTP header response, but with Puppet.
-exec { 'update':
-  command  => 'sudo apt-get update',
+exec {'update':
   provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['nginx intallation'],
 }
 
-package { 'nginx':
+exec {'nginx intallation':
   provider => shell,
   command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
 exec { 'add_header':
+  provider    => shell,
   environment => ["HOST=${hostname}"],
   command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  provider    => shell,
   before      => Exec['nginx restart'],
 }
 
 exec { 'nginx restart':
-  command  => 'sudo service nginx restart',
   provider => shell,
+  command  => 'sudo service nginx restart',
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-My first readme
+HTTPS SSL
