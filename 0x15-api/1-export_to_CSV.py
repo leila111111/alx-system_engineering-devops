8763748b68ed1325506f9e94708ffdab4f82999f
leila111111 <leila_merzak@outlook.fr>
2023-10-23 22:39:21 +0000

    tasks

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,8 +2,18 @@
 """extend  Python script that, using a REST API, for a given employee ID,
 returns information about his/her TODO list progress to export data in
 the CSV format."""
-import sys
-import requests
 import csv
+import requests
+from sys import argv
 
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    userId = argv[1]
+    user = requests.get(url + "users/{}".format(userId)).json()
+    todos = requests.get(url + "todos", params={"userId": userId}).json()
+    username = user.get("username")
+    with open("{}.csv".format(userId), "w", newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow(
+                [userId, username, todo.get("completed"), todo.get("title")])
