b2cff896192145b4d7372c23bc1860bf61624e13
leila111111 <leila_merzak@outlook.fr>
2023-10-23 22:47:57 +0000

    tasks

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 """extend the last Python script to export data in the JSON format"""
-import sys
-import requests
 import json
+import requests
+from sys import argv
 
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    userId = argv[1]
+    user = requests.get(url + "users/{}".format(userId)).json()
+    todos = requests.get(url + "todos", params={"userId": userId}).json()
+    username = user.get("username")
+    with open("{}.json".format(userId), "w") as f:
+        todo_list = []
+        for todo in todos:
+            todo_info = {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": username
+            }
+            todo_list.append(todo_info)
+        data = {userId: todo_list}
+        json.dump(data, f)
