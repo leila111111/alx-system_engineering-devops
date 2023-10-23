3586b68de94ace52145708cf9a79fa131c98f676
leila111111 <leila_merzak@outlook.fr>
2023-10-23 23:28:40 +0000

    tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """extend your Python script to export new data in the JSON format."""
-import requests
 import json
+import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
