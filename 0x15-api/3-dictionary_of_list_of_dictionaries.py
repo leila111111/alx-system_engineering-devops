ac8ffa4081dd0507c023bd6e0ccbfa71a5920632
leila111111 <leila_merzak@outlook.fr>
2023-10-23 23:27:21 +0000

    tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,3 +4,20 @@ import requests
 import json
 
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+    data = {}
+    for user in users:
+        userId = user.get("id")
+        username = user.get("username")
+        do_user = requests.get(url + "todos", params={"userId": userId}).json()
+        data[userId] = []
+        for todo in do_user:
+            todo_info = {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": username
+                }
+            data[userId].append(todo_info)
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(data, f)
