f7d0b7abefb74395b3973f88a9868fce091fb722
leila111111 <leila_merzak@outlook.fr>
2023-11-09 00:32:08 +0000

    task2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,13 +8,13 @@ import requests
 def recurse(subreddit, hot_list=[], after=""):
     """returns a list containing the titles of all
     hot articles for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,after)
     headers = {"User-Agent": "User"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return None
     results = response.json().get("data")
-    after = data.get("data").get("after")
+    after = results.get("after")
     hot_list.extend([i.get("data").get("title") for i in results.get("children")])
     if after is not None:
         recurse(subreddit, hot_list, after)
