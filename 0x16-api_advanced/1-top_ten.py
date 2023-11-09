5f6cafe64bc81ac012b95b9d9ae7ecc2856ed042
leila111111 <leila_merzak@outlook.fr>
2023-11-09 00:12:43 +0000

    task2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" function that queries the Reddit API and prints the titles
+of the first 10 hot posts"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts
+    listed for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "User"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(i.get("data").get("title")) for i in results.get("children")]
